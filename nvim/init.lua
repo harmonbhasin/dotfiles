d6a2552b4c8a25a95a127d9feccd38a3922d0b47
harmonbhasin <62266474+harmonbhasin@users.noreply.github.com>
2024-06-15 13:24:13 -0500

    Hard link instead of symbolic link

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+require("plugins")
+
+vim.wo.relativenumber = true
+vim.opt.tabstop = 2       -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 2    -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true  -- Use spaces instead of tabs
