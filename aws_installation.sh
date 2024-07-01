79b85ed6d2a57d19b8ea5238fa8376c304f41ecf
harmonbhasin <62266474+harmonbhasin@users.noreply.github.com>
2024-07-01 09:21:03 -0400

    Apparently you can't install neovim normally so you gotta do some funny business

--- aws_installation.sh
+++ aws_installation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -i
 
-sudo dnf install -y neovim python3-neovim
+#sudo dnf install -y neovim python3-neovim
+# Follow this on aws linux: https://gist.github.com/dreamorosi/f33906eb5b338ed50daeefad8f712921
 sudo dnf install -y tmux
