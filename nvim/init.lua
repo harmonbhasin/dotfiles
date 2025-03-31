c6eb30848bd403c208635ff38e76828f04e37f27
Harmon Bhasin <harmonprograms@protonmail.com>
2025-03-31 16:24:46 -0700

    remove symbolic link

---
+++ nvim/init.lua
@@ -0,0 +1,97 @@
+require("plugins")
+
+-- disable netrw at the very start of your init.lua (Nvim tree)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.wo.relativenumber = true
+vim.opt.tabstop = 2       -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 2    -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true  -- Use spaces instead of tabs
+vim.opt.syntax = "on"     -- Enable syntax highlighting
+vim.opt.termguicolors = true -- Enable 24-bit RGB color in the terminal
+vim.cmd 'colorscheme tokyonight-night' -- Add theme
+
+require("feline").setup()
+
+require("nvim-tree").setup()
+vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeOpen<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>t', ':tabnew<CR>', { noremap = true, silent = true })
+
+
+require("telescope").setup()
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require("render-markdown").setup()
+
+-- AVANTE CONFIG
+require("avante").setup({
+  provider = "claude",
+  claude = {
+    endpoint = "https://api.anthropic.com",
+    model = "claude-3-5-sonnet-20241022",
+    temperature = 0,
+    max_tokens = 4096,
+  }
+})
+
+-- iron
+require('my-iron')
+
+-- dashboard
+require('dashboard').setup{
+  theme='hyper'
+}
+
+-- LSP configuration
+require('lsp-config')
+
+-- Dap for python
+require('python-dap')
+
+-- TREE SITTER CONFIG
+require("nvim-treesitter.configs").setup{
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  -- List of parsers to ignore installing (or "all")
+  ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { "c", "rust" },
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
