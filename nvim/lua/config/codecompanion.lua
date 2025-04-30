e9f68f9571adc6f6bbd771fbad9e63907ccd3048
harm0n <harmonprograms@protonmail.com>
2025-04-29 21:37:50 -0400

    added newer models.

--- nvim/lua/config/codecompanion.lua
+++ nvim/lua/config/codecompanion.lua
@@ -1,4 +1,46 @@
 require("codecompanion").setup({
+	adapters = {
+		openai = function()
+			return require("codecompanion.adapters").extend("openai", {
+				name = "openai",
+				schema = {
+					model = {
+						default = "o4-mini",
+						choices = {
+							"gpt-4.1-mini",
+							"gpt-4.1-nano",
+							"gpt-4.1",
+							["o4-mini"] = { opt = { can_reason = true } },
+							["o3"] = { opt = { can_reason = true } },
+						},
+					},
+					max_completion_tokens = {
+						default = 200000,
+						optional = true,
+					},
+				},
+			})
+		end,
+		gemini = function()
+			return require("codecompanion.adapters").extend("gemini", {
+				name = "gemini",
+				schema = {
+					model = {
+						default = "gemini-2.5-pro-exp-03-25",
+						choices = {
+							["gemini-2.5-flash-preview-04-17"] = { opt = { can_reason = true } },
+							"gemini-2.5-pro-exp-03-25",
+							"gemini-2.0-flash",
+						},
+					},
+					max_completion_tokens = {
+						default = 500000,
+						optional = true,
+					},
+				},
+			})
+		end,
+	},
 	strategies = {
 		chat = {
 			adapter = "openai",
