59ba3eb9bcc1d15988f8a872cc48388aee6a68a5
harm0n <harmonprograms@protonmail.com>
2025-05-12 10:46:05 -0400

    idk

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -367,7 +367,7 @@ local plugins = {
 		opts = {},
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "z", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
