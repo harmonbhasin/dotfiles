213fec38c7f5104de2bb89ce0b52f4a634e44a71
harm0n <harmonprograms@protonmail.com>
2025-08-16 17:04:39 -0400

    remove autopairs from command line which is hella annoying

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -240,6 +240,18 @@ local plugins = {
   },
   {
     "m4xshen/autoclose.nvim",
+    config = function()
+      require("autoclose").setup({
+        options = {
+          disabled_filetypes = { "text" },
+          disable_when_touch = false,
+          touch_regex = "[%w(%[{]",
+          pair_spaces = false,
+          auto_indent = true,
+          disable_command_mode = true,
+        },
+      })
+    end,
   },
   {
     "f-person/git-blame.nvim",
